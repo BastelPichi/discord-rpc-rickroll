34fea0295c19ece55c5ff132a57c40e722538226
BastelPichi <63782569+BastelPichi@users.noreply.github.com>
2022-05-15 22:27:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ RPC.connect()
 
 
 i = 0
-
+print("Ready. Closing this Window will kill your Status, so leave it open ;)")
 while True:
     i += 1
     RPC.update(details="haha, I rickrolled you ;)", large_image=str(i), buttons=[{"label": "More Rickroll", "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"label": "Download this", "url": "https://rpcrickroll.ml/"}])
