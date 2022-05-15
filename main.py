596c50519f71947d0dc47e3f016990b302d1dbca
BastelPichi <63782569+BastelPichi@users.noreply.github.com>
2022-05-15 21:38:12 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+from pypresence import Presence
+
+client_id = "975411248105676901"
+RPC = Presence(client_id)
+RPC.connect()
+
+
+i = 0
+
+while True:
+    i += 1
+    RPC.update(details="haha, I rickrolled you ;)", large_image=str(i), buttons=[{"label": "More Rickroll", "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"label": "Download this", "url": "https://rpcrick.v6.rocks/"}])
+    if i > 8:
+        i = 0
+    time.sleep(1)
