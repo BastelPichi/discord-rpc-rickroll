753984f60a2e620486eb4ecb5c6691e411eb297c
BastelPichi <63782569+BastelPichi@users.noreply.github.com>
2022-12-26 15:54:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ i = 0
 print("Ready. Closing this Window will kill your Status, so leave it open ;)")
 while True:
     i += 1
-    RPC.update(details="haha, I rickrolled you ;)", large_image=str(i), buttons=[{"label": "More Rickroll", "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"label": "Download this", "url": "https://rpcrickroll.ml/"}])
+    RPC.update(details="haha, I rickrolled you ;)", large_image=str(i), buttons=[{"label": "More Rickroll", "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"label": "Download this", "url": "https://rpcrickroll.aj.do/"}])
     if i > 8:
         i = 0
     time.sleep(1)
